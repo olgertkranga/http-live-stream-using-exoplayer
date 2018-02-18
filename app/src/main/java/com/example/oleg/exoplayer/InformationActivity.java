4cfff0bb4eaa9f3dd07d62e1b25e8714ed1f0056
Edvins <olegs.kranga@inbox.lv>
2018-02-18 13:29:46 +0200

    Link to GitHub

--- app/src/main/java/com/example/oleg/exoplayer/InformationActivity.java
+++ app/src/main/java/com/example/oleg/exoplayer/InformationActivity.java
@@ -2,6 +2,9 @@ package com.example.oleg.exoplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class InformationActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class InformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        TextView text = (TextView) findViewById(R.id.text1);
+
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+
+        //String linkText = "Please look my GitHub <a href='https://github.com/olgertkranga/http-live-stream-using-exoplayer'>My GitHub</a> link.";
+        //textViewLink.setText(Html.fromHtml(linkText));
+        //textViewLink.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
